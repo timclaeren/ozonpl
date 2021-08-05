9a3ad3bfee4b7660f1c70672ef3f12b3871a4df4
TimClaeren <itim2@mail.ru>
2021-08-05 15:37:47 +0300

    Update ozonpl.go

--- ozonpl.go
+++ ozonpl.go
@@ -2,8 +2,6 @@ package main
 
 import "fmt"
 
-//import "fmt"
-
 type pist struct {
 	On bool
 	Ammo, Power int
@@ -11,34 +9,26 @@ type pist struct {
 
 func (c *pist) Shoot() bool{
 	var n bool
-	if c.Ammo >= 1{
+	if c.Ammo > 0 && c.On == true{
 		n = true
 		c.Ammo --
-	} else {n=false}
+	} else{n=false}
 	return n
 }
 func (c *pist) RideBike() bool{
 	var n bool
-	if c.Power >= 1{
+	if c.Power > 0 && c.On == true {
 		n = true
 		c.Power --
-	} else {n=false}
+	} else {n = false}
 	return n
 }
-func (c *pist) retry() bool{
-	var b bool
-	if c.Power >=1 && c.Ammo >=1{
-		b = true
-	} else {b=false}
-	return b
-}
+
+
 func main() {
-	//c := pist{true, 5, 5}
-
-	c := pist{true, 5, 5}
-	//testStruct := *new(pist)
-	fmt.Println(c)
-
+	c := pist{true, 1, 1}
+	testStruct := &c
+	fmt.Println(testStruct)
 }
 
 
