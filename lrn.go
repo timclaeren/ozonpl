2132f2e844b82a30f2c8300ffd497a016b74a447
TimClaeren <itim2@mail.ru>
2021-08-07 16:38:32 +0300

    .

--- lrn.go
+++ lrn.go
@@ -1,3 +1,29 @@
 package main
 
-text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+import (
+	"fmt"
+	"math"
+)
+
+var k float64 = 129
+var p float64 = -6
+var v float64 = -6
+
+func T() float64 {
+	n := W() / 6
+	return n
+}
+
+func W() float64 {
+	n := math.Sqrt(k / M())
+	return n
+}
+
+func M() float64 {
+	n := p * v
+	return n
+}
+
+func main() {
+	fmt.Println(T())
+}
